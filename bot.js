070898a3e2745369169ab0e202b35d7f23d088a3
Claudio Elias <claudioeliasf@gmail.com>
2025-03-14 13:52:09 -0300

    Atualizando arquivos do BOTLOTO

--- bot.js
+++ bot.js
@@ -46,8 +46,8 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        executablePath: puppeteer.executablePath(),
-        args: ["--no-sandbox", "--disable-setuid-sandbox"]
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
+        args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage", "--disable-accelerated-2d-canvas", "--no-first-run", "--no-zygote", "--single-process", "--disable-gpu"]
     }
 });
 
